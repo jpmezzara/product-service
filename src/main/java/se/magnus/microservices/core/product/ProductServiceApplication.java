8e8c2f27687caa1e70b431daf4d4095f846c3689
Juan Pablo Ramírez Mendoza <jpmezzara@me.com>
2020-03-21 21:18:35 -0600

    Product composite service

--- src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
+++ src/main/java/se/magnus/microservices/core/product/ProductServiceApplication.java
@@ -2,11 +2,17 @@ package se.magnus.microservices.core.product;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @ComponentScan("se.magnus")
 public class ProductServiceApplication {
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(ProductServiceApplication.class, args);
